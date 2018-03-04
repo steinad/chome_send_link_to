20351517c6732107110553bfa10e9cc5a5279db7
Aidan <stein.aidan@gmail.com>
2018-03-03 19:30:20 -0500

    post request goes to set url

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,10 +1,21 @@
+function sendLink(url, info) {
+	var xhr = new XMLHttpRequest();
+	if (!url) {
+		alert('No address specified!');
+		return;
+	}
+
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.send(JSON.stringify({url: info.linkUrl}));
+}
 chrome.contextMenus.removeAll();
 chrome.contextMenus.create({
 	title: "first",
 	contexts: ["link"],
 	onclick: function(info) {
-		console.log('testest');
-		var request = new XMLHttpRequest();
-		request.open("POST", "ff-debian-pc.home");
+		chrome.storage.sync.get('postAddr', function(items) {
+			sendLink(items.postAddr, info)
+		});
 	}
 });
